0800dd9d6b99fcd622e5cae7b4b70b6a1ffba04d
Jame <Jame@MSI>
2020-11-22 15:09:11 +0700

    fix num

--- work-28/Source.cpp
+++ work-28/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int* mypointer1;
 int* mypointer2;
-
+int a, b, row;
 void segregateEvenOdd(int nums[], int n)
 {
     int ctr = 0;
@@ -13,17 +13,17 @@ void segregateEvenOdd(int nums[], int n)
     }
 
     for (int i = 0; i < ctr; i++)
-        nums[i] = 0;
+        nums[i] = b;
 
     for (int i = ctr; i < n; i++)
-        nums[i] = 1;
+        nums[i] = a;
 }
 
 int main()
 {
-    int a,b,row;
-    cout << "Please enter two integers value that  must be 1,0 only: ";
-    scanf_s("%d", &a);//1
+    //int a,b,row;
+    cout << "Please enter two integers value  but one number must be 0: ";
+    scanf_s("%d", &a);//1 2 3 4 5 6
     scanf_s("%d", &b);//0
     mypointer1 = &a;
     mypointer2 = &b;
